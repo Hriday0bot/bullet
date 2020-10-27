1943881615ce56e784ffa2e504910d900c7b04e7
Hriday0bot <72862541+Hriday0bot@users.noreply.github.com>
2020-10-27 10:18:46 +0530

    Add files via upload

---
+++ bounceOffAlgorithm-master/bounceOffAlgorithm-master/sketch.js
@@ -0,0 +1,44 @@
+var bullet,wall;
+var wall,thickness;
+var bullet,speed,weight,damage;
+
+function setup(){
+createCanvas(1600,400);
+bullet  = createSprite(50,200,50,50);
+bullet.shapeColor = "white";
+bullet.velocityX = 9;
+wall = createSprite(1200,200,thickness,height/2);
+wall.shapeColor = "yellow";
+
+thickness  = random(22,83);
+speed = random(223,321);
+weight = random(30,52);
+}
+
+function draw(){
+  background(0,0,0);
+if(collided(bullet,wall)){
+  bullet.velocityX = 0;
+  var damage = 0.5 * weight * speed * speed/(thickness *thickness *thickness);
+ 
+ if(damage>10){
+wall.shapeColor=color(255,0,0);
+ }
+
+ if(gamage<10){
+wall.shapeColor = color(0,255,0)
+ }
+
+}
+
+createEdgeSprites();  
+drawSprites();
+}
+function collided(bullet,wall){
+bulletRightEdge = bullet.x+bullet.width;
+wallLeftEdge=wall.x;
+if(bulletRightEdge>=wallLeftEdge){
+  return true
+}
+return false
+}
